886c2c4072265b05b19063f183fc6b2593f764fd
DennyLazarova <denny.lazarova@gmail.com>
2024-07-30 17:07:54 +0300

    Add project files.

---
+++ HandlingFormInputs/SelectElement.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace HandlingFormInputs
+{
+    internal class SelectElement
+    {
+        private IWebElement webElement;
+
+        public SelectElement(IWebElement webElement)
+        {
+            this.webElement = webElement;
+        }
+
+        internal void SelectByText(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
